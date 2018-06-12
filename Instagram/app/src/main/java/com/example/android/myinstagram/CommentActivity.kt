632d57318534c6dca3a3a63fd5d5aa3b99524c51
mrThinBone <vinh.tv2003@gmail.com>
2018-06-12 13:51:53 +0700

    my instagram v1.3 comment button

--- Instagram/app/src/main/java/com/example/android/myinstagram/CommentActivity.kt
+++ Instagram/app/src/main/java/com/example/android/myinstagram/CommentActivity.kt
@@ -1,8 +1,11 @@
 package com.example.android.myinstagram
 
+import android.graphics.Rect
 import android.os.Bundle
+import android.os.Handler
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
+import android.view.ViewTreeObserver
 import com.example.android.myinstagram.adapter.CommentAdapter
 import com.example.android.myinstagram.anim.CommentItemAnimator
 import kotlinx.android.synthetic.main.activity_comment.*
@@ -10,6 +13,7 @@ import kotlinx.android.synthetic.main.activity_comment.*
 class CommentActivity: AppCompatActivity() {
 
     private var mAdapter: CommentAdapter? = null
+    private val handler = Handler()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,17 +25,41 @@ class CommentActivity: AppCompatActivity() {
         comment_list.itemAnimator = CommentItemAnimator()
         comment_list.adapter = mAdapter
 
+        btn_send.preCondition { edt_comment.text.isNotEmpty() }
         btn_send.setOnClickListener{
             val comment = edt_comment.text.toString()
             edt_comment.setText("")
             mAdapter?.addComment(comment)
             comment_list.scrollToPosition(mAdapter!!.itemCount-1)
+            handler.postDelayed({
+                btn_send.done()
+            }, 3000)
         }
     }
 
     override fun onResume() {
         super.onResume()
         comment_list.scrollToPosition(mAdapter!!.itemCount-1)
+        container.viewTreeObserver.addOnGlobalLayoutListener(globalLayoutListener)
     }
 
+    override fun onPause() {
+        super.onPause()
+        container.viewTreeObserver.removeOnGlobalLayoutListener(globalLayoutListener)
+    }
+
+    private val globalLayoutListener = object : ViewTreeObserver.OnGlobalLayoutListener {
+        private val rect = Rect()
+
+        override fun onGlobalLayout() {
+            container.getWindowVisibleDisplayFrame(rect)
+            val screenHeight = container.rootView.height
+
+            val keypadHeight = screenHeight - rect.bottom
+            if(keypadHeight > screenHeight * 0.15) {
+                comment_list.scrollToPosition(mAdapter!!.itemCount-1)
+            }
+        }
+
+    }
 }
--- Instagram/app/src/main/java/com/example/android/myinstagram/widget/SendCommentButton.kt
+++ Instagram/app/src/main/java/com/example/android/myinstagram/widget/SendCommentButton.kt
@@ -1,15 +1,56 @@
 package com.example.android.myinstagram.widget
 
 import android.content.Context
-import android.support.v7.widget.AppCompatButton
+import android.graphics.drawable.Animatable
+import android.support.v7.widget.AppCompatImageButton
 import android.util.AttributeSet
+import android.view.View
+import com.example.android.myinstagram.R
 
-class SendCommentButton: AppCompatButton {
+class SendCommentButton: AppCompatImageButton {
 
-    constructor(context: Context): super(context)
+    private val statePlay = intArrayOf(R.attr.state_send, -R.attr.state_loading, -R.attr.state_done)
+    private val stateLoading = intArrayOf(-R.attr.state_send, R.attr.state_loading, -R.attr.state_done)
+    private val stateDone = intArrayOf(-R.attr.state_send, -R.attr.state_loading, R.attr.state_done)
 
-    constructor(context: Context, attrs: AttributeSet): super(context, attrs)
+    private var sending = false
 
-    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int): super(context, attrs, defStyleAttr)
+    private var clickListener: View.OnClickListener? = null
+    private lateinit var triggerCondition: () -> Boolean
+
+    constructor(context: Context): this(context, null, 0)
+
+    constructor(context: Context, attrs: AttributeSet?): this(context, attrs, 0)
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int): super(context, attrs, defStyleAttr) {
+        setImageResource(R.drawable.asl_send_comment)
+        super.setOnClickListener {
+            if(!sending && triggerCondition()) {
+                sending = true
+                clickListener?.onClick(it)
+                setImageResource(R.drawable.avd_three_dots)
+                (drawable as Animatable).start()
+            }
+        }
+    }
+
+    fun preCondition(condition: () -> Boolean) {
+        triggerCondition = condition
+    }
+
+    override fun setOnClickListener(l: OnClickListener?) {
+        clickListener = l
+    }
+
+    fun done() {
+        (drawable as Animatable).stop()
+        setImageResource(R.drawable.asl_send_comment)
+        setImageState(stateLoading, true)
+        setImageState(stateDone, true)
+        postDelayed({
+            setImageState(statePlay, true)
+            sending = false
+        }, 500)
+    }
 
 }
